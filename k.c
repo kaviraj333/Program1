5738c0cae5125dfc680b357c249d8218c125900d
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-04-23 15:57:36 +0530

    k.c

---
+++ k.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ {
+   int a,b,c;
+   clrscr();
+   printf("Enter the numbers:");
+   scanf("%d%d%d",&a,&b,&c);
+   if(a>b)
+    {
+     printf("The largest number is:%d",a);
+     }
+     else 
+      {
+      printf("the largest number is:%d",b);
+      }
+      else if(b>c)
+       {
+       printf("The largest number is:%d",c);
+       }
+       getch();
+      } 
