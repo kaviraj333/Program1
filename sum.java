accbecc7671576590262b3c427dde68e1ccd0d09
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-04-23 16:39:14 +0530

    sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Sum
+{
+public static void main(String args[])
+int sum=0;
+int n=2;
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+System.out.println("the sum of "+n" number are:
++sum);
+}
+}
