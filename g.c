263365f28947d401a873e22748c42b23e88cfdc7
kaviraj333 <49511634+kaviraj333@users.noreply.github.com>
2019-04-23 15:41:49 +0530

    g.c

---
+++ g.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ {
+   char n; 
+   clrscr();
+   printf("Enter the letter:");
+   scanf('%c',&n);
+   printf("The first alphabet letter:");
+   getch();
+   }
